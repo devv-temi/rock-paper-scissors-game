31acf0732ade507c22bfb12c1926d0df1c5ac7a2
James Balogun <jamesbalogun085@gmail.com>
2024-06-20 18:17:22 +0100

    updated

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const compScore = 0;
+let userScore = 0;
+let compScore = 0;
 const userScore_span = document.getElementById("user-score");
 const compScore_span = document.getElementById("comp-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_p = document.querySelector(".result > p");
+const result_h1 = document.querySelector(".result > h1");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -17,62 +17,67 @@ function getComputerChoice() {
 function convertToWord(letter) {
     if (letter === "r") return "Rock";
     if (letter === "p") return "Paper";
-    return "Scissors";
+    if (letter === "s") return "Scissors";
 }
 
 function win(userChoice, computerChoice) {
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallcompWord = "comp".fontsize(3).sub();
     userScore++;
     userScore_span.innerHTML = userScore;
     compScore_span.innerHTML = compScore;
-    result_p.innerHTML = `${convertToWord(userChoice)} beats {convertToWord(computerChoice). You win!}`;
+    result_h1.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(computerChoice)}. You win`;
 } 
 
-function lose() {
-
+function lose(userChoice, computerChoice) {
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallcompWord = "comp".fontsize(3).sub();
+    compScore++;
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
+    result_h1.innerHTML = `${convertToWord(userChoice)} loses to ${convertToWord(computerChoice)}. You lost`;
 }
 
-function draw() {
-
+function draw(userChoice, computerChoice) {
+    // userScore++;
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallcompWord = "comp".fontsize(3).sub();
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
+    result_h1.innerHTML = `${convertToWord(userChoice)} equals ${convertToWord(computerChoice)}. It is a tie`;
 }
 
-// rock_div.addEventListener('click', playGame)
-// paper_div.addEventListener('click', playGame)
-// scissors_div.addEventListener('click', playGame)
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    switch (userChoice +computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win(userChoice, computerChoice);
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            lose(userChoice, computerChoice);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw(userChoice, computerChoice);
+            break;
+    }
+}
 
-// function getWinner(playerChoice, computerChoice) {
-//     if (playerChoice === computerChoice) {
-//         return "It is a draw!"
-//     } else if (
-//     (playerChoice === "r" && computerChoice === "s") || 
-//     (playerChoice === "p" && computerChoice === "r") || 
-//     (playerChoice === "s" && computerChoice === "p")) {
-//         return "You win!"
-//     } else {
-//         return "You lose!"
-//     }
-// }
+function mainGame() {
+    rock_div.addEventListener('click', function() {
+        game("r")
+    })
+    paper_div.addEventListener('click', function() {
+        game("p")
+    })
+    scissors_div.addEventListener('click', function() {
+        game("s")
+    })
+}
 
-// function playGame () {
-
-// }
-
-// function getWinner(playerChoice, computerChoice) {
-//   if (playerChoice === computerChoice) {
-//     return "It's a tie!";
-//   } else if (
-//     (playerChoice === 'rock' && computerChoice === 'scissors') ||
-//     (playerChoice === 'paper' && computerChoice === 'rock') ||
-//     (playerChoice === 'scissors' && computerChoice === 'paper')
-//   ) {
-//     return "You win!";
-//   } else {
-//     return "Computer wins!";
-//   }
-// }
-
-// function playGame(e) {
-//   const playerChoice = e.target.id;
-//   const computerChoice = getComputerChoice();
-//   const result_div = getWinner(playerChoice, computerChoice);
-//   scoreBoard_div.textContent = `You chose ${playerChoice}, computer chose ${computerChoice}. ${scoreBoard_div}`;
-// }
+mainGame()
